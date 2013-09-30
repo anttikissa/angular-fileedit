4e088f87677d7b098cc8432875a38a66add6bdf3
Antti Sykäri <antti.sykari@gmail.com>
2013-09-30 15:41:26 +0300

    Change code formatting style.  (Some Monday productivity here.)

--- client/app/views/directoryentryview.js
+++ client/app/views/directoryentryview.js
@@ -1,24 +1,27 @@
-define(['backbone', 'underscore', 'util', 'app'], function(Backbone, _, util, app) {
-	var DirectoryEntryView = Backbone.View.extend({
-		template: _.template(app.templates.filename),
+define(
+	['backbone', 'underscore', 'util', 'app'],
+	function(Backbone, _, util, app, template) {
+		var DirectoryEntryView = Backbone.View.extend({
+			template: _.template(app.templates.filename),
 
-		initialize: function() {
-			this.listenTo(this.model, 'change', this.render);
-		},
+			initialize: function() {
+				this.listenTo(this.model, 'change', this.render);
+			},
 
-		tagName: 'li',
+			tagName: 'li',
 
-		render: function() {
-			var data = this.model.toJSON();
-			// TODO ugly, store path as a separate piece of data
-			var parentPath = this.model.collection.url.replace('/files', '');
-			_.extend(data, {
-				path: util.joinPaths(parentPath, this.model.get('name'))
-			});
-			this.$el.html(this.template(data));
-			return this;
-		}
-	});
+			render: function() {
+				var data = this.model.toJSON();
+				// TODO ugly, store path as a separate piece of data
+				var parentPath = this.model.collection.url.replace('/files', '');
+				_.extend(data, {
+					path: util.joinPaths(parentPath, this.model.get('name'))
+				});
+				this.$el.html(this.template(data));
+				return this;
+			}
+		});
 
-	return DirectoryEntryView;
-});
+		return DirectoryEntryView;
+	}
+);
